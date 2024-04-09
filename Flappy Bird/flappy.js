e21ed098b9e62c63f2a73204c52bf54cdfae82e2
Maxprraaa <paracosas2018@gmail.com>
2024-04-09 16:20:28 +0200

    controlar creacion y movimiento tuberia

--- Flappy Bird/flappy.js
+++ Flappy Bird/flappy.js
@@ -5,24 +5,20 @@ window.addEventListener("load", () => {
 	var divGameOver = document.getElementById("gameOver");
 	var divScoreText = document.getElementById("score_text");
 	var divScore_Puntos = document.getElementById("score_puntos");
-	var gravedad = 0.08;
+	var gravedad = 0.02;
 	var velocidadY = 0;
-	var VelocidadX = 1.5;
 	var salto = false;
-var muerto = false;
+	var muerto = false;
 	function generarTuberias() {
-		var espacioEntreTuberias = 50;
+		var espacioEntreTuberias = 75;
 		var anchoTuberia = 100;
-		var AlturaTuberia = 200;
+		var AlturaTuberia = 100;
 
 		var divTuberiaSuperior = document.createElement("div");
 		var divTuberiaInferior = document.createElement("div");
 
 		divTuberiaSuperior.className = "tuberia";
 		divTuberiaInferior.className = "tuberia";
-
-		divTuberiaInferior.style.zIndex = 1;
-		divTuberiaSuperior.style.zIndex = 1;
 
 		divTuberiaSuperior.style.width = anchoTuberia + "px";
 		divTuberiaInferior.style.width = anchoTuberia + "px";
@@ -34,8 +30,7 @@ var muerto = false;
 			) + "px";
 		divTuberiaInferior.style.height =
 			window.innerHeight -
-			parseInt(divTuberiaSuperior.style.height) -
-			espacioEntreTuberias +
+			parseInt(divTuberiaSuperior.style.height) +
 			"px";
 
 		divTuberiaSuperior.style.position = "absolute";
@@ -44,8 +39,10 @@ var muerto = false;
 		divTuberiaSuperior.style.top = "0";
 		divTuberiaInferior.style.bottom = "0";
 
+
 		document.body.appendChild(divTuberiaSuperior);
 		document.body.appendChild(divTuberiaInferior);
+		
 	}
 
 	document.addEventListener("keydown", function (event) {
@@ -62,13 +59,12 @@ var muerto = false;
 		var codigoTecla = event.keyCode || event.which;
 		// Detecta la tecla para iniciar el juego
 		if (codigoTecla == 13 && !iniciado) {
-			
-				iniciado = true;
-				divMensaje.innerHTML = "";
-				divScore_Puntos.innerHTML = "0";
-				divScoreText.innerHTML = "Score: ";
-				requestAnimationFrame(animarPajaro);
-		}else if (codigoTecla == 13 && iniciado && muerto){
+			iniciado = true;
+			divMensaje.innerHTML = "";
+			divScore_Puntos.innerHTML = "0";
+			divScoreText.innerHTML = "Score: ";
+			requestAnimationFrame(animarPajaro);
+		} else if (codigoTecla == 13 && iniciado && muerto) {
 			iniciado = false;
 			muerto = false;
 			divScore_Puntos.innerHTML = "0";
@@ -78,12 +74,12 @@ var muerto = false;
 			divGameOver.innerHTML = "";
 		}
 		//  Movimiento salto
-		else if (iniciado && (codigoTecla == 32 || codigoTecla == 38) && !salto) {
+		else if (iniciado && (codigoTecla == 32 || codigoTecla == 38) && !salto & !muerto) {
 			salto = true;
 			velocidadY = 0;
 			imgPajaro.style.top = imgPajaro.offsetTop - 75 + "px";
 			if (imgPajaro.offsetTop <= 0) {
-				imgPajaro.style.top = 5 + "px";
+				imgPajaro.style.top = 10 + "px";
 			}
 			setTimeout(function () {
 				salto = false;
@@ -119,5 +115,3 @@ var muerto = false;
 
 	setInterval(generarTuberias, 2000);
 });
-
-
